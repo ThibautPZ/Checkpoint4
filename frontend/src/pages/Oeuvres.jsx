4ae71b96795ed01f6ec881f9d0f1ea1e792045a1
Thibaut Plez <thibautplez@gmail.com>
2023-09-30 00:22:09 +0200

    feat: created Oeuvres page to display all paintings in the db

--- frontend/src/pages/Oeuvres.jsx
+++ frontend/src/pages/Oeuvres.jsx
@@ -1,5 +1,33 @@
+import { useEffect, useState } from "react";
+import axiosInstance from "../services/axiosInstance";
+import OeuvresList from "../components/OeuvresList";
+
+import "../scss/Oeuvres.scss";
+
 function Oeuvres() {
-  return <div>Oeuvres</div>;
+  const [oeuvresList, setOeuvresList] = useState([]);
+
+  const fetchAllOeuvres = async () => {
+    try {
+      const paintingsList = await axiosInstance.get(`/api/paintings/`);
+
+      setOeuvresList(paintingsList.data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchAllOeuvres();
+  }, []);
+
+  return (
+    <div className="Oeuvres">
+      <h1>Toutes mes oeuvres</h1>
+
+      <OeuvresList oeuvresList={oeuvresList} />
+    </div>
+  );
 }
 
 export default Oeuvres;
